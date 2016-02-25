88e6f7c0e60c11c12efc978db74b85990e6af47b
Reinout van Rees <reinout@vanrees.org>
2016-02-25 11:04:30 +0100

    Adjusted for renamed function

--- serverscripts/tests/test_nginx.py
+++ serverscripts/tests/test_nginx.py
@@ -15,22 +15,22 @@ class GitAndEggInfoTestCase(TestCase):
             our_dir, 'example_multiple_nginx_proxy.conf')
 
     def test_single_count(self):
-        result = list(nginx.site_info(self.single_example))
+        result = list(nginx.extract_sites(self.single_example))
         print(result)
         self.assertEquals(len(result), 1)
 
     def test_multiple_count(self):
         # Two server parts, one with a double name.
-        result = list(nginx.site_info(self.multiple_example))
+        result = list(nginx.extract_sites(self.multiple_example))
         print(result)
         self.assertEquals(len(result), 3)
 
     def test_protocol_http(self):
-        result = list(nginx.site_info(self.single_example))
+        result = list(nginx.extract_sites(self.single_example))
         print(result)
         self.assertEquals(result[0]['protocol'], 'http')
 
     def test_protocol_https(self):
-        result = list(nginx.site_info(self.multiple_example))
+        result = list(nginx.extract_sites(self.multiple_example))
         print(result)
         self.assertEquals(result[0]['protocol'], 'https')
