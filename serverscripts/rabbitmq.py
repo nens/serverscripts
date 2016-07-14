957306eec7bf146c927a81b1015f38623a0dd545
AlexandrSeleznev <a.seleznev@hotmail.nl>
2016-07-14 05:32:24 -0700

    Add validations, tests.

--- serverscripts/rabbitmq.py
+++ serverscripts/rabbitmq.py
@@ -20,6 +20,11 @@ CONFIG_FILE = os.path.join(CONFIG_DIR, 'rabbitmq_zabbix.json')
 OUTPUT_DIR = '/var/local/serverinfo-facts'
 OUTPUT_FILE = os.path.join(OUTPUT_DIR, 'rabbitmq.fact')
 
+SUCCEEDED = 'SUCCEEDED'
+FAILED = 'FAILED'
+
+QUEUES_LIMIT = 'queues_limit'
+MESSAGES_LIMIT = 'messages_limit'
 
 logger = logging.getLogger(__name__)
 
@@ -40,31 +45,83 @@ def parse_queues_stdout(queues_stdout):
     queues = {}
     for line in queues_stdout.split('\n'):
         line_attrs = line.split()
-        print(line_attrs)
         if len(line_attrs) == 2:
             queues[line_attrs[0]] = int(line_attrs[1])
     return queues
 
 
 def retrieve_queues(vhost):
-    """Run shell command en return the queues."""
-    result = subprocess.check_output(
-        ['rabbitmqctl', 'list_queues', '-p', str(vhost)])
-    if result:
-        return result
-    else:
-        logger.warn("%s vhost is not available or has not any queue.")
-        return ''
+    """Run shell command en return a tuple with status and stdout."""
+    status = SUCCEEDED
+    stdout = ''
+    try:
+        stdout = subprocess.check_output(
+            ['rabbitmqctl', 'list_queues', '-p', str(vhost)])
+    except OSError:
+        status = FAILED
+        stdout = "rabbitmqctl is not available."
+    except subprocess.CalledProcessError:
+        status = FAILED
+        stdout = "'rabbitmqctl list_queues -p %s' returns non-zero exit status." % vhost 
+    
+    return (status, stdout)
 
 
 def get_max_queue(queues):
-    """Retrieve a queue with max messages as tulp."""
-    return max(queues.iteritems(), key=operator.itemgetter(1))
+    """Retrieve a queue with max messages as tuple."""
+    queue, value = max(queues.iteritems(), key=operator.itemgetter(1))
+    return (queue, value)
 
 
 def load_config(config_file_path):
     with open(config_file_path, 'r') as config_file:
-        return json.loads(config_file.read())
+        try:
+            content = json.loads(config_file.read())
+            status = SUCCEEDED
+        except:
+            content = 'Can not load a rabbitmq-zabbix configuration.'
+            logger.exception(content)
+            status = FAILED
+        return (status, content)            
+
+
+def cast_to_int(value):
+    try:
+        return int(value)
+    except:
+        return None
+
+
+def validate_configuration(configuration):
+    """Validate loaded content of rabbitmq-zabbix.json."""
+    error_type = "Rabbitmq-Zabbix configuration error:"
+    if not configuration:
+        logger.error("%s: no vhost.", error_type)
+        return False
+
+    for vhost in configuration:
+        queues_limit_value = configuration[vhost].get(QUEUES_LIMIT)
+        messages_limit_value = configuration[vhost].get(MESSAGES_LIMIT)
+        queues_limit_key = configuration[vhost].has_key(QUEUES_LIMIT)
+        messages_limit_key = configuration[vhost].has_key(MESSAGES_LIMIT)
+
+        if not queues_limit_key:
+            logger.error("%s: vhost '%s' has not '%s' item.",
+                         error_type, vhost, QUEUES_LIMIT)
+            return False
+        if not messages_limit_key:
+            logger.error("%s: vhost '%s' has not '%s' item.",
+                         error_type, vhost, MESSAGES_LIMIT)
+            return False
+        if not cast_to_int(queues_limit_value):
+            logger.error("%s: '%s'.'%s' is not an integer.",
+                         error_type, vhost, QUEUES_LIMIT)
+            return False
+        if not cast_to_int(messages_limit_value):
+            logger.error("%s: '%s'.'%s' is not an integer.",
+                         error_type, vhost, MESSAGES_LIMIT)            
+            return False
+    return True
 
 
 def main():
@@ -106,21 +163,40 @@ def main():
     if not os.path.isfile(CONFIG_FILE):
         return
 
-    rabbitmq_config = load_config(CONFIG_FILE)
-    logger.info("VHOST to check %d" % len(rabbitmq_config))
-    vhosts = rabbitmq_config.keys()
-    result = {}
-    for vhost in vhosts:
-        queues_stdout = retrieve_queues(vhost)
-        logger.debug("Queues stdout: %s" % queues_stdout)
-        queues = parse_queues_stdout(queues_stdout)
-        max_queue = get_max_queue(queues)
-        if max_queue[1] >= rabbitmq_config[vhost]['messages']:
-            result[vhost] = {max_queue[0]: max_queue[1]}
-        else:
-            continue
+    status, configuration = load_config(CONFIG_FILE)
+    if status == FAILED:
+        logger.error("Exit due configuration error %s" % configuration)
+        return
 
-    logger.info("Check results to dump: %d." % len(result))
-    open(OUTPUT_FILE, 'w').write(json.dumps(result, sort_keys=True, indent=4))
+    is_valid_configuration = validate_configuration(configuration)
+    if not is_valid_configuration:
+        logger.error("Exit due invalid configuration")
+        return
+
+    results = {}
+    for vhost in configuration:
+        status, stdout = retrieve_queues(vhost)
+        logger.debug("Result of rabbitmqctl for vhost %s: status %s, stdout %s" % (
+            vhost, status, stdout))
+        if status == FAILED:
+            results[vhost] = {'messsage': stdout}
+            continue
+        queues = parse_queues_stdout(stdout)
+        # check or the vhost has a queue
+        if len(queues) <= 0:
+            results[vhost] = {'messsage': 'The vhost has any queue or not exists'}
+            continue
+        # check the allowed amount of queues per vhost
+        if len(queue) >= configuration[vhost][QUEUES_LIMIT]:
+            results[vhost] = {'messsage': 'The limit of queues is reached'}
+            continue
+        # check the allowed amount of messages in the largest queue 
+        queue_name, queue_value = get_max_queue(queues)
+        if queue_value >= configuration[vhost][MESSAGES_LIMIT]:
+            results[vhost] = {
+                'message': 'The limit of messages in queue "%s" is reached' % max_queue[0]}
+
+    logger.info("Write check results to files: %d." % len(results))
+    open(OUTPUT_FILE, 'w').write(json.dumps(results, sort_keys=True, indent=4))
     zabbix_file = os.path.join(VAR_DIR, 'nens.rabbitmq.warnings')
-    open(zabbix_file, 'w').write(len(results))
+    open(zabbix_file, 'w').write('%d' % len(results))
--- serverscripts/tests/test_rabbitmq.py
+++ serverscripts/tests/test_rabbitmq.py
@@ -12,6 +12,8 @@ class GitAndEggInfoTestCase(TestCase):
         our_dir = os.path.dirname(__file__)
         self.config_example = os.path.join(
             our_dir, 'example_rabbitmq_zabbix.json')
+        self.broken_config_example = os.path.join(
+            our_dir, 'example_rabbitmq_zabbix_broken.json')
         self.queues_stdout_example = os.path.join(
             our_dir, 'example_queues_stdout_example.txt')
 
@@ -32,6 +34,59 @@ class GitAndEggInfoTestCase(TestCase):
         self.assertEquals(result, test_case)
 
     def test_load_config_file(self):
-        config_data = rabbitmq.load_config(self.config_example)
-        pprint(config_data)
-        self.assertEquals(len(config_data), 2)
+        status, content = rabbitmq.load_config(self.config_example)
+        self.assertEquals((status, isinstance(content, dict)), (rabbitmq.SUCCEEDED, True))
+
+    def test_load_config_file_broken_content(self):
+        status, content = rabbitmq.load_config(self.broken_config_example)
+        self.assertEquals(status, rabbitmq.FAILED)
+
+    def test_rabbitmqctl_not_exists(self):
+        vhost = 'asdasda'
+        status, result = rabbitmq.retrieve_queues(vhost)
+        self.assertEquals(status, rabbitmq.FAILED)
+
+    def test_validate_configuration_empty(self):
+        configuration = {}
+        is_valid =  rabbitmq.validate_configuration(configuration)
+        self.assertFalse(is_valid)
+
+    def test_validate_configuration_no_messages_limit(self):
+        configuration = {
+            'flooding': {'queues_limit': 23},
+            'lizard-nxt': {'queues_limit': 1, 'messages_limit': 200}
+        }
+        is_valid =  rabbitmq.validate_configuration(configuration)
+        self.assertFalse(is_valid)
+
+    def test_validate_configuration_no_queues_limit(self):
+        configuration = {
+            'flooding': {'queues_limit': 23, 'messages_limit': 200},
+            'lizard-nxt': {'messages_limit': 200}
+        }
+        is_valid =  rabbitmq.validate_configuration(configuration)
+        self.assertFalse(is_valid)
+
+    def test_validate_configuration_queues_not_int(self):
+        configuration = {
+            'flooding': {'queues_limit': 'dfdf', 'messages_limit': 200},
+            'lizard-nxt': {'messages_limit': '200', 'queues_limit': 1}
+        }
+        is_valid =  rabbitmq.validate_configuration(configuration)
+        self.assertFalse(is_valid)
+
+    def test_validate_configuration_messages_not_int(self):
+        configuration = {
+            'flooding': {'queues_limit': 22, 'messages_limit': ''},
+            'lizard-nxt': {'messages_limit': 200, 'queues_limit': 1}
+        }
+        is_valid =  rabbitmq.validate_configuration(configuration)
+        self.assertFalse(is_valid)
+
+    def test_validate_configuration_valid(self):
+        configuration = {
+            'flooding': {'queues_limit': 22, 'messages_limit': 22},
+            'lizard-nxt': {'messages_limit': '20', 'queues_limit': 1}
+        }
+        is_valid =  rabbitmq.validate_configuration(configuration)
+        self.assertTrue(is_valid)
